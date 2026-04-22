c41714b8f31d135abaabdeadeba2f32efaa19de5
Surendra26 <yanamadalasurendra@gmai.com>
2026-04-22 10:19:55 +0530

    fix: handle missing pymysql gracefully for local environment

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,7 @@
-import pymysql
-
-# Mocking a newer mysqlclient version to satisfy Django's requirements
-pymysql.version_info = (2, 2, 1, "final", 0)
-pymysql.install_as_MySQLdb()
+try:
+    import pymysql
+    # Mocking a newer mysqlclient version to satisfy Django's requirements
+    pymysql.version_info = (2, 2, 1, "final", 0)
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
